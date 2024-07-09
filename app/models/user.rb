10a05d83e05995455baeeeb784ef082dd545e8de
YYsvg <rara10202001@gmail.com>
2024-07-09 21:50:08 +0900

    create_line_login

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,omniauth_providers: %i[line]
+
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
+
+  def set_valurs(omniauth)
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
+
+    access_token = credentilas["refresh_token"]
+    access_secret = credentials["secret"]
+    credentials = credentials.to_json
+    self.name = info["name"]
+  end
+
+end
